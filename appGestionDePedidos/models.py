9f1a823b142ef676ffae76dd9b3ca9a2e0ee5d3c
AiserVG <79530917+AiserVG@users.noreply.github.com>
2021-04-17 16:35:17 +0200

    añado el models.py

--- appGestionDePedidos/models.py
+++ appGestionDePedidos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    id_cliente = models.CharField(max_length=5)
+    cif = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    telefono = models.IntegerField(max_length=9)
+    nombreEmpresa = models.CharField(max_length=50)
+    email = models.EmailField(max_legth=50)
+
+class Pedido(models.Model):
+    id_pedido = models.CharField(max_length=5)
+    fecha = models.CharField(max_length=50)
+    precioTotal = models.IntegerField(max_length=10)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) #para la recacion 1-n con cliente
+    producto = models.ManyToManyField(Producto) #esto nos crea la tabla intermedia de la n-m invisible de pedido-producto
+
+class Producto(models.Model):
+    id_producto = models.CharField(max_length=5)
+    precio = models.IntegerField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    componente = models.ManyToManyField(Componente) #esto nos crea la tabla intermedia de la n-m invisible producto-componente
+
+class Componente(models.Model):
+    id_componente= models.CharField(max_length=5)
+    nombre = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    
