74da9928ca1b888410224f4c811c896874f3d326
AiserVG <79530917+AiserVG@users.noreply.github.com>
2021-04-19 19:24:53 +0200

    añadir import os en setings.py y corregir un par de fallos en models.py

--- appGestionDePedidos/models.py
+++ appGestionDePedidos/models.py
@@ -7,25 +7,7 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     telefono = models.IntegerField(max_length=9)
     nombreEmpresa = models.CharField(max_length=50)
-    email = models.EmailField(max_legth=50)
-
-    def __str__(self):
-        return self.nombre
-
-class Pedido(models.Model):
-    id_pedido = models.CharField(max_length=5)
-    fecha = models.CharField(max_length=50)
-    precioTotal = models.IntegerField(max_length=10)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) #para la recacion 1-n con cliente
-    producto = models.ManyToManyField(Producto) #esto nos crea la tabla intermedia de la n-m invisible de pedido-producto
-
-class Producto(models.Model):
-    id_producto = models.CharField(max_length=5)
-    precio = models.IntegerField(max_length=10)
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=50)
-    categoria = models.CharField(max_length=50)
-    componente = models.ManyToManyField(Componente) #esto nos crea la tabla intermedia de la n-m invisible producto-componente
+    email = models.EmailField(max_length=50)
 
     def __str__(self):
         return self.nombre
@@ -37,3 +19,25 @@ class Componente(models.Model):
     
     def __str__(self):
         return self.nombre
+        
+class Producto(models.Model):
+    id_producto = models.CharField(max_length=5)
+    precio = models.IntegerField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    componente = models.ManyToManyField(Componente) #esto nos crea la tabla intermedia de la n-m invisible producto-componente
+
+    def __str__(self):
+        return self.nombre
+        
+class Pedido(models.Model):
+    id_pedido = models.CharField(max_length=5)
+    fecha = models.CharField(max_length=50)
+    precioTotal = models.IntegerField(max_length=10)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) #para la recacion 1-n con cliente
+    producto = models.ManyToManyField(Producto) #esto nos crea la tabla intermedia de la n-m invisible de pedido-producto
+
+
+
+
