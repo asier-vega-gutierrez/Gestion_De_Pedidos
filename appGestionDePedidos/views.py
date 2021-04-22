d5bdbe8386eae9bf3d9cb3c9ecc2b4841ec79977
ikersebastian <iker.sebastian@opendeusto.es>
2021-04-22 19:00:00 +0200

    css,html,index... prueba

--- appGestionDePedidos/views.py
+++ appGestionDePedidos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Cliente, Componente, Producto, Pedido
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, "base.html", context)
+
+#class ProductoListView(ListView):
+
+#class ProductoDetailView(DetailView):
+
+#class PedidoListView(ListView):
+
+#class PedidoDetailView(DetailView):
