2f82f4f94e23c1ad54d00fddeee35d13c82a437e
ikersebastian <iker.sebastian@opendeusto.es>
2021-04-23 16:35:07 +0200

    views, nuevo html falta acabar, models

--- appGestionDePedidos/views.py
+++ appGestionDePedidos/views.py
@@ -10,10 +10,33 @@ def pagPrincipal(request):
     context = {}
     return render(request, "pagPrincipal.html", context)
 
-#class ProductoListView(ListView):
+class ProductoListView(ListView):
 
-#class ProductoDetailView(DetailView):
+        model = Producto
+        template_name = 'ListaProductos.html'
+        context_object_name = 'lista_productos'
 
-#class PedidoListView(ListView):
+class ProductoDetailView(DetailView):
 
-#class PedidoDetailView(DetailView):
+        model = Producto
+        #template_name = 
+
+class PedidoListView(ListView):
+
+        model = Pedido
+        #template_name = 
+
+class PedidoDetailView(DetailView):
+
+        model = Pedido
+        #template_name = 
+    
+class ClienteListView(ListView):
+
+        model = Cliente
+        #template_name = 
+
+class ComponenteListView(ListView):
+
+        model = Componente
+        #template_name = 
