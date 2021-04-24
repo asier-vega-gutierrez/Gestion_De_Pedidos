280ea5641678fbbc4378845e6a464ed59b4a1c02
Sandra Quintana Ferret <sandra.quintana@opendeusto.es>
2021-04-24 19:25:30 +0200

    arreglo BBDD e intento de dejarlo igual al último commit (style no actualizado)

--- appGestionDePedidos/models.py
+++ appGestionDePedidos/models.py
@@ -35,6 +35,9 @@ class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) #para la recacion 1-n con cliente
     producto = models.ManyToManyField(Producto) #esto nos crea la tabla intermedia de la n-m invisible de pedido-producto
 
+    def __str__(self):
+        return self.fecha
+
 
 
 
