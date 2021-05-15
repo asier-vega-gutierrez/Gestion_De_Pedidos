53a67888a96f23b44a77d836a8060ce4a5fa3ef1
ikersebastian <iker.sebastian@opendeusto.es>
2021-05-15 14:46:44 +0200

    otro intento barra busqueda

--- appGestionDePedidos/static/JS/script.js
+++ appGestionDePedidos/static/JS/script.js
@@ -1,6 +1,17 @@
-let producto = document.getElementById("IdProducto");
+function esperarResultadosIncorrectos(){
 
-    if(producto.textContent =="")
-    {
-        producto.append(<p>No se han encontrado resultados</p>)
-    }
+    let divi = document.getElementById('Busqueda');
+
+    let producto = document.getElementsByClassName("Prueba");
+
+    let aviso = document.createElement("div");
+
+    aviso.innerHTML = (`<p>No se han encontrado resultados</p>`);
+
+        if(!(producto.length>0))
+        {
+            divi.append(aviso);
+        }
+}
+
+document.addEventListener("load", esperarResultadosIncorrectos())
