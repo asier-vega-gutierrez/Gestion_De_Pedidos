e348569692eda19d2bf2ba18b032732efc286ee7
Sandra Quintana Ferret <sandra.quintana@opendeusto.es>
2021-05-21 21:11:53 +0200

    arreglo validarRegistro

--- appGestionDePedidos/static/JS/validarRegistro.js
+++ appGestionDePedidos/static/JS/validarRegistro.js
@@ -24,21 +24,20 @@ const camposFormulario = {
 /*Creamos la funcion de tipo arrow validarFormulario, que llama a la funcion validarCampo de una manera automatica, independientemente del campo*/
 const validarFormulario = (e) => {
     switch (e.target.name){
-
+        case "username":
+            validarCampo(expresiones.usuario, e.target, 'username');
+        break;
         case "first_name":
-            validarCampo(expresiones.nombre, e.target, 'Nombre');
+            validarCampo(expresiones.nombre, e.target, 'first_name');
         break;
         case "last_name":
-            validarCampo(expresiones.apellido, e.target, 'Apellido');
-        break;
-        case "username":
-            validarCampo(expresiones.usuario, e.target, 'Usuario');
+            validarCampo(expresiones.apellido, e.target, 'last_name');
         break;
         case "email":
-            validarCampo(expresiones.email, e.target, 'Email');
+            validarCampo(expresiones.email, e.target, 'email');
         break;
         case "password1":
-            validarCampo(expresiones.password, e.target, 'Password1');
+            validarCampo(expresiones.password, e.target, 'password1');
             validarPassword2();
         break;
         case "password2":
@@ -50,35 +49,38 @@ const validarFormulario = (e) => {
 /*Creamos la funcion de tipo arrow validarCampo, que añade o elimina una clase al div de dicho campo con el fin de mostra un determinado icono*/
 const validarCampo = (expresion, input, campo ) => {
     if (expresion.test(input.value)) {
-        document.getElementById(`div${campo}`).classList.remove('formulario_incorrecto');
-        document.getElementById(`div${campo}`).classList.add('formulario_correcto');
-        document.querySelector(`#div${campo} i`).classList.remove('fa-times-circle');
-        document.querySelector(`#div${campo} i`).classList.add('fa-check-circle');
+        document.getElementById(`div_${campo}`).classList.remove('formulario_incorrecto');
+        document.getElementById(`div_${campo}`).classList.add('formulario_correcto');
+        document.querySelector(`#div_${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#div_${campo} i`).classList.add('fa-check-circle');
+        camposFormulario[campo] = true;
 
     } else {
-        document.getElementById(`div${campo}`).classList.add('formulario_incorrecto');
-        document.getElementById(`div${campo}`).classList.remove('formulario_correcto');
-        document.querySelector(`#div${campo} i`).classList.remove('fa-check-circle');
-        document.querySelector(`#div${campo} i`).classList.add('fa-times-circle');
+        document.getElementById(`div_${campo}`).classList.add('formulario_incorrecto');
+        document.getElementById(`div_${campo}`).classList.remove('formulario_correcto');
+        document.querySelector(`#div_${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#div_${campo} i`).classList.add('fa-times-circle');
+        camposFormulario[campo] = false;
     }
 }
 
-//*Creamos la funcion de tipo arrow validarPassword2,, para validar que la password1 es igual a la password 2 en todo momento*/
+//*Creamos la funcion de tipo arrow validarPassword2, para validar que la password1 es igual a la password 2 en todo momento*/
 const validarPassword2 = () => {
     const inputPass1 = document.getElementById('id_password1');
     const inputPass2 = document.getElementById('id_password2');
 
     if(inputPass1.value !== inputPass2.value) {
-        document.getElementById(`divPassword2`).classList.add('formulario_incorrecto');
-        document.getElementById(`divPassword2`).classList.remove('formulario_correcto');
-        document.querySelector(`#divPassword2 i`).classList.remove('fa-check-circle');
-        document.querySelector(`#divPassword2 i`).classList.add('fa-times-circle');
+        document.getElementById(`div_password2`).classList.add('formulario_incorrecto');
+        document.getElementById(`div_password2`).classList.remove('formulario_correcto');
+        document.querySelector(`#div_password2 i`).classList.remove('fa-check-circle');
+        document.querySelector(`#div_password2 i`).classList.add('fa-times-circle');
+
         camposFormulario['password1'] = false;
     } else {
-        document.getElementById(`divPassword2`).classList.remove('formulario_incorrecto');
-        document.getElementById(`divPassword2`).classList.add('formulario_correcto');
-        document.querySelector(`#divPassword2 i`).classList.remove('fa-times-circle');
-        document.querySelector(`#divPassword2 i`).classList.add('fa-check-circle');
+        document.getElementById(`div_password2`).classList.remove('formulario_incorrecto');
+        document.getElementById(`div_password2`).classList.add('formulario_correcto');
+        document.querySelector(`#div_password2 i`).classList.remove('fa-times-circle');
+        document.querySelector(`#div_password2 i`).classList.add('fa-check-circle');
         camposFormulario['password1'] = true;
     }
 }
@@ -90,16 +92,13 @@ inputsFormulario.forEach((input) => {
 });
 
 /*Añadimos un EventListener al formulario para que no permita enviar los datos si la validacion no es correcta*/
-Formulario.addEventListener('submit', prueba)
-
-function prueba(){
-    console.log('hola')
-}
-/*, (e) => {
-    if (camposFormulario.username && camposFormulario.first_name && camposFormulario.last_name && camposFormulario.email && camposFormulario.password1) {
+Formulario.addEventListener('submit', (e) => {
+    alert(camposFormulario.username)
+    
+    if (camposFormulario.username && camposFormulario.first_name && camposFormulario.last_name && camposFormulario.email && camposFormulario.password1 == true) {
 
     } else {
         alert('Alguno de los campos no era correcto. Por favor, vuelva a intentarlo');
         Formulario.reset();
     }
-});*/
+});
